c9c686519d92842776f6f0b625b26fab6e364173
lyricallll <lachariotte30@gmail.com>
2025-01-02 12:13:05 +0100

    lib

--- include/pipex.h
+++ include/pipex.h
@@ -6,13 +6,14 @@
 /*   By: agraille <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/02 08:15:48 by agraille          #+#    #+#             */
-/*   Updated: 2025/01/02 08:17:04 by agraille         ###   ########.fr       */
+/*   Updated: 2025/01/02 11:37:54 by agraille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_H
 # define PIPEX_H
 
-#include "../libft/include/libft.h"
+# include "../libft/include/libft.h"
+# include "../printf/include/ft_printf.h"
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,13 +6,19 @@
 /*   By: agraille <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/02 08:14:58 by agraille          #+#    #+#             */
-/*   Updated: 2025/01/02 08:17:54 by agraille         ###   ########.fr       */
+/*   Updated: 2025/01/02 11:42:49 by agraille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+// acces verfie si permissio dacceder au fichier ou repertoire
 
 #include "../include/pipex.h"
 
 int	main(void)
 {
+	fork();
+	fork();
+	fork();
+	ft_printf("Hello World\n");
 	return (0);
 }
